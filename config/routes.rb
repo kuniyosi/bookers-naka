86cdc63c75b986e1c1033a1b7c6baced0365919f
nakazawa <zhongzejiangji@gmail.com>
2023-06-23 02:47:12 +0000

    books index show add

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :books, only:[:new, :edit, :show, :index]
+  resources :books
 
   resources :users, only:[:new, :edit, :show, :index]
 
