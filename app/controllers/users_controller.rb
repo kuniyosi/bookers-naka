a55ae4ca6d0d37a0d091e3a76095e4ee4a4904e3
nakazawa <zhongzejiangji@gmail.com>
2023-06-23 04:20:26 +0000

    user index show add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,17 @@
 class UsersController < ApplicationController
-  def new
-  end
 
   def edit
   end
 
   def index
+    @book_new = Book.new
+    @user = current_user
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @book_new = Book.new
+    @books = @user.books
   end
 end
